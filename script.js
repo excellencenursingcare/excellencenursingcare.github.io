55b4bffc45c3edb92f972f07b9d978936e617463
Cruz Cardenas <cruz.cardenas@vanco.com>
2023-08-12 12:32:42 -0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById('contactForm').addEventListener('submit', function (event) {
+    event.preventDefault();
+    // You can add your form submission logic here
+    // For this example, we are just preventing the form from submitting
+    alert('Form submitted successfully!');
+  });
+  
